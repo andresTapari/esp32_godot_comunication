ff09425aa547ee00a2148804d642ee13cdcc5810
andresTapa <79794569+andresTapari@users.noreply.github.com>
2023-08-10 13:27:24 -0300

    actualizacion comunicacion

---
+++ esp32_firmware/definitions.h
@@ -0,0 +1,14 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+// RED WIFI a donde conectar
+/*
+const char* ssid     = "Fibertel WiFi190 2.4GHz";       // Reemplaza con tu SSID de red WiFi
+const char* password = "0043616130";                    // Reemplaza con tu contraseña de red WiFi
+const int udpPort    = 12000;                           // Reemplazar por puerto  usar
+*/
+const char* ssid     = "Wifi_IAPV";       // Reemplaza con tu SSID de red WiFi
+const char* password = "lapri351";                    // Reemplaza con tu contraseña de red WiFi
+const int udpPort    = 12000;                           // Reemplazar por puerto  usar
+
+#endif
--- esp32_firmware/esp32_firmware.ino
+++ esp32_firmware/esp32_firmware.ino
@@ -1,41 +1,48 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
+#include "definitions.h"
 
 WiFiUDP udp;
+/*
+  const char* ssid = "Fibertel WiFi190 2.4GHz";       // Reemplaza con tu SSID de red WiFi
+  const char* password = "0043616130";                // Reemplaza con tu contraseña de red WiFi
+  const int udpPort = 12000;
+*/
 
-const char* ssid = "Fibertel WiFi190 2.4GHz";       // Reemplaza con tu SSID de red WiFi
-const char* password = "0043616130";                // Reemplaza con tu contraseña de red WiFi
-const int udpPort = 12000;
-
-void setup() {
-    WiFi.begin(ssid, password);
-    Serial.begin(115200);
-    Serial.print("Conectando a WiFi ");
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-      Serial.print(".");
-      delay(1000);
-    }
+void setup()
+{
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+  Serial.print("Conectando a WiFi ");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
   Serial.println("");
   Serial.println("Conexión WiFi establecida");
   Serial.print("Dirección IP: ");
   Serial.println(WiFi.localIP());
-  udp.begin(udpPort);                                 // Reemplaza 'udpPort' con el número de puerto que desees usar
+  udp.begin(udpPort); 
 }
 
-void loop() 
+void loop()
 {
   int packetSize = udp.parsePacket();
-  if (packetSize) 
+  if (packetSize)
   {
     char packetData[255];
     int bytesRead = udp.read(packetData, sizeof(packetData));
-    if (bytesRead > 0) 
+    if (bytesRead > 0)
     {
+      // Mostrar los datos del paquete en el monitor serie
+      Serial.print("Datos del paquete: ");
+      Serial.println(packetData);
+      
       // Procesa los datos recibidos, por ejemplo, envía una respuesta
       udp.beginPacket(udp.remoteIP(), udp.remotePort());
-      char* message = "Respuesta desde ESP32";
-      udp.write((uint8_t*)message, strlen(message));
+      char *message = "Respuesta desde ESP32";
+      udp.write((uint8_t *)message, strlen(message));
       udp.endPacket();
     }
   }

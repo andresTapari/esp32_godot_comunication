f9307cbe2d3abb8e69e175ef582accf91eddc72c
andresTapa <79794569+andresTapari@users.noreply.github.com>
2023-08-30 06:17:03 -0300

    Update

---
+++ godot_project/scenes/Ejemplo3/Ejemplo3.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var esp32 = ESP32.new()
+
+
+func _ready():
+	esp32.attachServo(0,19)
+	esp32.attachServo(0,23)
+
+func _input(event):
+	if event is InputEventJoypadMotion:
+		var turn_axis: float = Input.get_axis("ui_right", "ui_left")
+		var speed: float = Input.get_axis("ui_up", "ui_down")
+		handle_speed_turn_axis(turn_axis,speed)
+	if event is InputEventJoypadButton:
+		# button_index = 6
+		print("ESP Write")
+		esp32.digitalWrite(18,true)
+		esp32.attachServo(19,0)
+		esp32.attachServo(23,1)
+func handle_speed_turn_axis(_turn_axis: float, _speed: float) -> void:
+	var turnEn  = _turn_axis != 0 
+	var turnDir = _turn_axis < 0
+	
+	var turnSpeed = map_value(abs(_turn_axis),0.0,1.0,0,100) 
+	var speed = map_value(abs(_speed),0.0,1.0,0,100) 
+	
+	if not turnEn:
+		esp32.servoWrite(speed,0,_speed > 0)
+		esp32.servoWrite(speed,1,_speed < 0)
+	else:
+		esp32.servoWrite(turnSpeed,0,turnDir)
+		esp32.servoWrite(turnSpeed,1,turnDir)
+	
+	print("Turn En:",turnEn ," | Speed:",speed , "| TurnSpeed:", turnSpeed)
+
+func map_value(value, from_min, from_max, to_min, to_max):
+	# Asegurarse de que el valor esté dentro del rango de entrada
+	value = max(from_min, min(from_max, value))
+	# Calcular el porcentaje del valor en el rango de entrada
+	var normalized_value = (value - from_min) / (from_max - from_min)
+	# Calcular el valor mapeado en el rango de salida
+	var mapped_value = to_min + normalized_value * (to_max - to_min)
+	# Retornamos valor 
+	return mapped_value
--- godot_project/scripts/classDefinitions/Esp32.gd
+++ godot_project/scripts/classDefinitions/Esp32.gd
@@ -1,28 +1,58 @@
 extends Node
 class_name ESP32
 
+# digitalWrite(pinNumber: int, value: bool) -> void
+# Esta función permite enviar comandos para establecer el valor digital (alto o 
+# bajo) de un pin específico.
+#
+# El comando se envía en forma de mensaje a través de un servidor UDP (User 
+# Datagram Protocol) para controlar dispositivos externos o realizar acciones 
+# en base a la señal de un pin.
+#
+# Parámetros:
+# - pinNumber (int): El número del pin que se desea controlar.
+# - value (bool): El valor que se quiere establecer en el pin (True para alto, False para bajo).
 
 func digitalWrite(pinNumber:int , value: bool) -> void:
-	# "$|1|pinNumber|value|0|0|0"
 	var strValue = "0"
 	if value:
 		strValue = "1"
 	var message = "$" + "|1|" + str(pinNumber) + "|" + strValue + "|0|0|0"
 	if UdpServer.isConnected:
+		# "$|1|pinNumber|value|0|0|0"
 		UdpServer.send_to_client(message)
+
+# attachServo(pinNumber: int, servoNumber: int) -> void
+# Esta función permite enviar un mensaje para adjuntar un servo motor a un pin específico.
+# El mensaje se envía a través de un servidor UDP (User Datagram Protocol) para configurar y controlar
+# el servo motor en dispositivos externos.
+#
+# Parámetros:
+# - pinNumber (int): El número del pin al que se desea adjuntar el servo motor.
+# - servoNumber (int): El número o identificador del servo motor que se está configurando.
 
 func attachServo(pinNumber:int, servoNumber: int) -> void:
-	# "$|2|pinNumber|servoNumber|0|0|0"
 	var message = "$" + "|2|" + str(pinNumber) + "|" + str(servoNumber) + "|0|0|0"
-	print(message)
 	if UdpServer.isConnected:
+		# "$|2|pinNumber|servoNumber|0|0|0"
 		UdpServer.send_to_client(message)
 
+# servoWrite(angle_in_degrees: int, servoNumber: int, foward: bool = True) -> void
+# Esta función envía un mensaje para controlar la posición de un servo motor adjunto a un pin 
+# específico.
+# El mensaje se envía a través de un servidor UDP (User Datagram Protocol) para 
+# mover el servo a un ángulo determinado.
+#
+# Parámetros:
+# - angle_in_degrees (int): El ángulo en grados al que se desea mover el servo motor.
+# - servoNumber (int): El número o identificador del servo motor que se va a controlar.
+# - foward (bool): Indica si el movimiento es hacia adelante (True) o hacia 
+# atrás (False). Valor predeterminado: True.
+
 func servoWrite(angle_in_degrees: int, servoNumber: int, foward: bool = true ) -> void:
-	# "$|3|angle_in_degrees|servoNumber|foward|0|0"
 	var direction: int = (foward == true)
 	var message = "$" + "|3|" + str(angle_in_degrees) + "|" + str(servoNumber) \
 				+ "|"+str(direction) +"|0|0"
-#	print(message)
 	if UdpServer.isConnected:
+		# "$|3|angle_in_degrees|servoNumber|foward|0|0"
 		UdpServer.send_to_client(message)
